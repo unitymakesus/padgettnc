c2b3bfbfeca8cc7ac41ee4c2c61f3740f6078793
Alisa Herr <alisa@unitymakes.us>
2018-08-24 12:47:57 -0400

    Make navbar accessible

--- wp-content/themes/padgettnc/resources/views/partials/header.blade.php
+++ wp-content/themes/padgettnc/resources/views/partials/header.blade.php
@@ -54,6 +54,6 @@
     <label for="menu-trigger"><i class="material-icons" aria-label="Show navigation menu">menu</i></a>
   </div>
   @if (has_nav_menu('primary_navigation'))
-    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-menu']) !!}
   @endif
 </nav>
