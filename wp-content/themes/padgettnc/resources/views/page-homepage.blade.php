62ec3c313b972c7af23b0231847f20d10056ee4a
lexinamer <lexinamer@gmail.com>
2018-10-30 10:30:04 -0400

    fixing CTA on homepage header

--- wp-content/themes/padgettnc/resources/views/page-homepage.blade.php
+++ wp-content/themes/padgettnc/resources/views/page-homepage.blade.php
@@ -18,7 +18,7 @@
         <span class="subheader">{!! $hero['subheader'] !!}</span>
       </h1>
 
-      <a class="btn btn-getstarted" href="{!! $cta['cta_link'] !!}">{{ $cta['cta_text'] }}</a>
+      <a class="btn btn-getstarted" href="{!! $cta['cta_link'] !!}">Find out how!</a>
     </div>
 
   </header>
@@ -168,7 +168,7 @@
       <div class="cta" style="background-image: url({!! $cta['image'] !!})">
         <img style="width: 400px; margin-bottom: 1em;" src="{{ $cta['cta_got_image'] }}" alt="Profit is coming"/>
         <!-- <h2>{{ $cta['header'] }}</h2> -->
-        <a class="btn btn-getstarted" href="{!! $cta['cta_link'] !!}">Find out how!</a>
+        <a class="btn btn-getstarted" href="{!! $cta['cta_link'] !!}">{{ $cta['cta_text'] }}</a>
       </div>
       @endif
 
